eb071dbd9131ec28546156755b6de3ea40c34e52
Will Glynn <will@willglynn.com>
2015-10-07 17:46:06 -0500

    Use unsafe/reflect in serialization
    Serialized representations are just little endian byte slices, which can be
    obtained by referencing the data slice directly on certain platforms.

--- bitmapcontainer.go
+++ bitmapcontainer.go
@@ -10,24 +10,6 @@ type bitmapContainer struct {
 	bitmap      []uint64
 }
 
-// writes the content
-func (b *bitmapContainer) writeTo(stream io.Writer) (int, error) {
-	// Write set
-	buf := make([]byte, 8*len(b.bitmap))
-	for i, v := range b.bitmap {
-		base := i * 8
-		buf[base] = byte(v)
-		buf[base+1] = byte(v >> 8)
-		buf[base+2] = byte(v >> 16)
-		buf[base+3] = byte(v >> 24)
-		buf[base+4] = byte(v >> 32)
-		buf[base+5] = byte(v >> 40)
-		buf[base+6] = byte(v >> 48)
-		buf[base+7] = byte(v >> 56)
-	}
-	return stream.Write(buf)
-}
-
 func (b *bitmapContainer) readFrom(stream io.Reader) (int, error) {
 	err := binary.Read(stream, binary.LittleEndian, b.bitmap)
 	if err != nil {
---
+++ serialization_littleendian.go
@@ -0,0 +1,43 @@
+// +build 386 amd64,!appengine
+
+package roaring
+
+import (
+	"io"
+	"reflect"
+	"unsafe"
+)
+
+func (b *arrayContainer) writeTo(stream io.Writer) (int, error) {
+	buf := uint16SliceAsByteSlice(b.content)
+	return stream.Write(buf)
+}
+
+func (b *bitmapContainer) writeTo(stream io.Writer) (int, error) {
+	buf := uint64SliceAsByteSlice(b.bitmap)
+	return stream.Write(buf)
+}
+
+func uint64SliceAsByteSlice(slice []uint64) []byte {
+	// make a new slice header
+	header := *(*reflect.SliceHeader)(unsafe.Pointer(&slice))
+
+	// update its capacity and length
+	header.Len *= 8
+	header.Cap *= 8
+
+	// return it
+	return *(*[]byte)(unsafe.Pointer(&header))
+}
+
+func uint16SliceAsByteSlice(slice []uint16) []byte {
+	// make a new slice header
+	header := *(*reflect.SliceHeader)(unsafe.Pointer(&slice))
+
+	// update its capacity and length
+	header.Len *= 2
+	header.Cap *= 2
+
+	// return it
+	return *(*[]byte)(unsafe.Pointer(&header))
+}
