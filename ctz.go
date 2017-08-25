1b73104229d35e389617230ed584c09cbe8d879f
Maciej Bilas <maciej.bilas@gmail.com>
2017-08-25 11:19:43 +0300

    countTrailingZeros -> bits.TrailingZeros64

--- ctz.go
+++ ctz.go
@@ -1,5 +1,7 @@
 package roaring
 
+import "math/bits"
+
 // Reuse of portions of go/src/math/big standard lib code
 // under this license:
 /*
@@ -32,38 +34,8 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-const deBruijn32 = 0x077CB531
-
-var deBruijn32Lookup = []byte{
-	0, 1, 28, 2, 29, 14, 24, 3, 30, 22, 20, 15, 25, 17, 4, 8,
-	31, 27, 13, 23, 21, 19, 16, 7, 26, 12, 18, 6, 11, 5, 10, 9,
-}
-
-const deBruijn64 = 0x03f79d71b4ca8b09
-
-var deBruijn64Lookup = []byte{
-	0, 1, 56, 2, 57, 49, 28, 3, 61, 58, 42, 50, 38, 29, 17, 4,
-	62, 47, 59, 36, 45, 43, 51, 22, 53, 39, 33, 30, 24, 18, 12, 5,
-	63, 55, 48, 27, 60, 41, 37, 16, 46, 35, 44, 21, 52, 32, 23, 11,
-	54, 26, 40, 15, 34, 20, 31, 10, 25, 14, 19, 9, 13, 8, 7, 6,
-}
-
 // trailingZeroBits returns the number of consecutive least significant zero
 // bits of x.
-func countTrailingZerosDeBruijn(x uint64) int {
-	// x & -x leaves only the right-most bit set in the word. Let k be the
-	// index of that bit. Since only a single bit is set, the value is two
-	// to the power of k. Multiplying by a power of two is equivalent to
-	// left shifting, in this case by k bits. The de Bruijn constant is
-	// such that all six bit, consecutive substrings are distinct.
-	// Therefore, if we have a left shifted version of this constant we can
-	// find by how many bits it was shifted by looking at which six bit
-	// substring ended up at the top of the word.
-	// (Knuth, volume 4, section 7.3.1)
-	if x == 0 {
-		// We have to special case 0; the fomula
-		// below doesn't work for 0.
-		return 64
-	}
-	return int(deBruijn64Lookup[((x&-x)*(deBruijn64))>>58])
+func countTrailingZeros(x uint64) int {
+	return bits.TrailingZeros64(x)
 }
