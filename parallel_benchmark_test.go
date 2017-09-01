fbd138d361a7f3550d701fb486a184656faa982f
Maciej Bilas <maciej.bilas@gmail.com>
2017-09-01 15:48:17 +0200

    Size the result channel differently, slightly improves perf
    ```
    BenchmarkIntersectionSparseParallel-8   	    1000	   1210170 ns/op
    BenchmarkIntersectionSparseRoaring-8    	     300	   4289596 ns/op
    ```
    same late 2013 quad-core i7 15' MBP

--- parallel_benchmark_test.go
+++ parallel_benchmark_test.go
@@ -90,7 +90,16 @@ func (aggregator ParAggregator) And(x1, x2 *Bitmap) *Bitmap {
 	length1 := x1.highlowcontainer.size()
 	length2 := x2.highlowcontainer.size()
 
-	resultChan := make(chan parResult, 64)
+	var chanLength int
+	// take smaller of two input bitmap lengths
+	// this makes the buffer large enough not to block the workers
+	if length1 > length2 {
+		chanLength = length2
+	} else {
+		chanLength = length1
+	}
+
+	resultChan := make(chan parResult, chanLength)
 	resultCount := 0
 
 main:
@@ -99,13 +108,14 @@ main:
 		s2 := x2.highlowcontainer.getKeyAtIndex(pos2)
 		for {
 			if s1 == s2 {
-				C := x1.highlowcontainer.getContainerAtIndex(pos1)
+				left := x1.highlowcontainer.getContainerAtIndex(pos1)
+				right := x2.highlowcontainer.getContainerAtIndex(pos2)
 
 				aggregator.taskQueue <- parTask{
 					op:     parOpAnd,
 					pos:    resultCount,
-					left:   C,
-					right:  x2.highlowcontainer.getContainerAtIndex(pos2),
+					left:   left,
+					right:  right,
 					result: resultChan,
 				}
 				resultCount++
