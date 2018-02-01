b96b65cb78bf7fe4820793800811ff22d06bd23b
Maciej Bilas <maciej.bilas@gmail.com>
2018-02-01 16:08:12 +0100

    real_data_benchmark: somehow faster reading of bitmap files

--- real_data_benchmark_test.go
+++ real_data_benchmark_test.go
@@ -4,7 +4,7 @@ import (
 	"archive/zip"
 	"bytes"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"os"
 	"path"
 	"strconv"
@@ -54,24 +54,50 @@ func retrieveRealDataBitmaps(datasetName string, optimize bool) ([]*Bitmap, erro
 	}
 	defer zipFile.Close()
 
+	var largestFileSize uint64
+	for _, f := range zipFile.File {
+		if f.UncompressedSize64 > largestFileSize {
+			largestFileSize = f.UncompressedSize64
+		}
+	}
+
 	bitmaps := make([]*Bitmap, len(zipFile.File))
+	buf := make([]byte, largestFileSize)
+	var bufStep uint64 = 32768 // apparently the largest buffer zip can read
 	for i, f := range zipFile.File {
 		r, err := f.Open()
 		if err != nil {
 			return nil, fmt.Errorf("failed to read bitmap file %s from dataset %s, cause: %v", f.Name, datasetName, err)
 		}
 
-		content, err := ioutil.ReadAll(r)
-		if err != nil {
-			r.Close()
-			return nil, fmt.Errorf("could not read content of file %s from dataset %s, cause: %v", f.Name, datasetName, err)
+		var totalReadBytes uint64
+
+		for {
+			var endOffset uint64
+			if f.UncompressedSize64 < totalReadBytes+bufStep {
+				endOffset = f.UncompressedSize64
+			} else {
+				endOffset = totalReadBytes + bufStep
+			}
+
+			readBytes, err := r.Read(buf[totalReadBytes:endOffset])
+			totalReadBytes += uint64(readBytes)
+
+			if err == io.EOF {
+				r.Close()
+				break
+			} else if err != nil {
+				r.Close()
+				return nil, fmt.Errorf("could not read content of file %s from dataset %s, cause: %v", f.Name, datasetName, err)
+			}
 		}
 
-		elemsAsBytes := bytes.Split(content, []byte{44}) // 44 is a comma
+		elemsAsBytes := bytes.Split(buf[:totalReadBytes], []byte{44}) // 44 is a comma
 
 		b := NewBitmap()
 		for _, elemBytes := range elemsAsBytes {
 			elemStr := strings.TrimSpace(string(elemBytes))
+
 			e, err := strconv.ParseUint(elemStr, 10, 32)
 			if err != nil {
 				r.Close()
@@ -80,13 +106,12 @@ func retrieveRealDataBitmaps(datasetName string, optimize bool) ([]*Bitmap, erro
 
 			b.Add(uint32(e))
 		}
+
 		if optimize {
 			b.RunOptimize()
 		}
 
 		bitmaps[i] = b
-
-		r.Close()
 	}
 
 	return bitmaps, nil
