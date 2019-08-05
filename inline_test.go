1ae358661350d9a9b5ca9b4d726cf96fbc78ef5e
Jackson Newhouse <jnewhouse@quantcast.com>
2019-08-04 22:45:12 -0700

    some renames

--- inline_test.go
+++ inline_test.go
@@ -3,30 +3,30 @@ package roaring
 import "testing"
 
 //hack to return a container with just the 0 value in the container.
-func UnitZeroContainer(test *testing.T) container {
+func ZeroContainer(test *testing.T) container {
 	bytes := make([]byte, 4)
 	return &arrayContainer{byteSliceAsUint16Slice(bytes[2:])}
 }
 
 func inlinedFunction(test *testing.T) (container, []container) {
-	populationContainer := UnitZeroContainer(test)
+	populationContainer := ZeroContainer(test)
 	observationContainers := make([]container, 0)
-	container:= UnitZeroContainer(test)
+	container:= ZeroContainer(test)
 	observationContainers = append(observationContainers, container)
 	return populationContainer, observationContainers
 }
 
 func TestHundredTimes(test *testing.T) {
 	for i := 0; i < 100; i++ {
-		testUnits(test)
+		testComparison(test)
 	}
 }
 
-func testUnits(test *testing.T) {
-	pop, obs := inlinedFunction(test)
+func testComparison(test *testing.T) {
+	a, b := inlinedFunction(test)
 	//this line is apparently required?
-	test.Logf("produced: %+v,%+v", pop, obs[0])
-	if !pop.equals(obs[0]) {
-		test.Errorf("unexpected:%+v,%+v", pop, obs[0])
+	test.Logf("produced: %+v,%+v", a, b[0])
+	if !a.equals(b[0]) {
+		test.Errorf("unexpected:%+v,%+v", a, b[0])
 	}
 }
