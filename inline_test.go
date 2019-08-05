396c8d8b371b3714b3eb6b496a8422095156e61d
Jackson Newhouse <jnewhouse@quantcast.com>
2019-08-04 22:49:01 -0700

    comments.

--- inline_test.go
+++ inline_test.go
@@ -5,6 +5,8 @@ import "testing"
 //hack to return a container with just the 0 value in the container.
 func ZeroContainer(test *testing.T) container {
 	bytes := make([]byte, 4)
+	//if you uncomment this, the test passes
+	//test.Logf("produced: %+v", bytes)
 	return &arrayContainer{byteSliceAsUint16Slice(bytes[2:])}
 }
 
@@ -24,7 +26,7 @@ func TestHundredTimes(test *testing.T) {
 
 func testComparison(test *testing.T) {
 	a, b := inlinedFunction(test)
-	//this line is apparently required?
+	// if you comment out this line the test passes
 	test.Logf("produced: %+v,%+v", a, b[0])
 	if !a.equals(b[0]) {
 		test.Errorf("unexpected:%+v,%+v", a, b[0])
