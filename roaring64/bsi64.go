2ee925e4706710b8b5fd14bdeaa4d05e5afc8d0f
Daniel Lemire <lemire@gmail.com>
2021-05-27 12:19:02 -0400

    Typo.

--- roaring64/bsi64.go
+++ roaring64/bsi64.go
@@ -632,7 +632,7 @@ func (b *BSI) addDigit(foundSet *Bitmap, i int) {
 	}
 	carry := And(b.bA[i], foundSet)
 	b.bA[i].Xor(foundSet)
-	if !carry.isEmpty() {
+	if !carry.IsEmpty() {
 		if i+1 >= len(b.bA) {
 			b.bA = append(b.bA, NewBitmap())
 		}
