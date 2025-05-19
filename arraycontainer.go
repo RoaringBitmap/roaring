d7108b2da29892257d76e0107da93ec392cffb09
Abderrezak Kamel <akamel@demandbase.com>
2025-05-19 15:28:03 -0700

    Fixed a bug with AndAny, a case of uint16 overflow

--- arraycontainer.go
+++ arraycontainer.go
@@ -966,8 +966,8 @@ func (ac *arrayContainer) resetTo(a container) {
 		ac.realloc(card)
 		cur := 0
 		for _, r := range x.iv {
-			for val := r.start; val <= r.last(); val++ {
-				ac.content[cur] = val
+			for val := int(r.start); val <= int(r.last()); val++ {
+				ac.content[cur] = uint16(val)
 				cur++
 			}
 		}
