4184f91e2faef0fe6e35968538a677c6ceb75130
Tomas Valenta <valenta.and.thomas@gmail.com>
2026-04-10 13:25:11 -0700

    Add Ranges iterator
    Add Ranges() method that iterates contiguous [start, end) ranges of set bits efficiently, without walking individual values. Ranges spanning container boundaries are merged.

--- iter.go
+++ iter.go
@@ -42,3 +42,108 @@ func Unset(b *Bitmap, min, max uint32) iter.Seq[uint32] {
 		}
 	}
 }
+
+// Ranges iterates contiguous ranges of values present in the bitmap as
+// half-open [start, endExclusive) pairs. endExclusive is uint64 to represent
+// ranges that include MaxUint32. Ranges spanning container boundaries are merged.
+func (b *Bitmap) Ranges() iter.Seq2[uint32, uint64] {
+	return func(yield func(uint32, uint64) bool) {
+		ra := &b.highlowcontainer
+		keys := ra.keys
+		containers := ra.containers
+		n := len(keys)
+
+		var pendingStart, pendingEnd uint64
+		hasPending := false
+
+		emit := func(rStart, rEnd uint64) bool {
+			if hasPending && rStart <= pendingEnd {
+				if rEnd > pendingEnd {
+					pendingEnd = rEnd
+				}
+				return true
+			}
+			if hasPending {
+				if !yield(uint32(pendingStart), pendingEnd) {
+					return false
+				}
+			}
+			pendingStart = rStart
+			pendingEnd = rEnd
+			hasPending = true
+			return true
+		}
+
+		for idx := 0; idx < n; idx++ {
+			hs := uint64(keys[idx]) << 16
+			c := containers[idx]
+
+			switch t := c.(type) {
+			case *runContainer16:
+				for _, iv := range t.iv {
+					if !emit(hs+uint64(iv.start), hs+uint64(iv.start)+uint64(iv.length)+1) {
+						return
+					}
+				}
+
+			case *bitmapContainer:
+				bm := t.bitmap
+				length := uint(len(bm))
+				pos := uint(0)
+
+				for pos < length {
+					if bm[pos] == 0 {
+						pos++
+						continue
+					}
+
+					w := bm[pos]
+					lo := uint(countTrailingZeros(w))
+					bitStart := pos*64 + lo
+
+					ones := uint(countTrailingOnes(w >> lo))
+					if lo+ones < 64 {
+						if !emit(hs|uint64(bitStart), hs|uint64(bitStart+ones)) {
+							return
+						}
+						pos = (bitStart + ones) / 64
+					} else {
+						pos++
+						for pos < length && bm[pos] == 0xFFFFFFFFFFFFFFFF {
+							pos++
+						}
+						var bitEnd uint
+						if pos < length {
+							bitEnd = pos*64 + uint(countTrailingOnes(bm[pos]))
+						} else {
+							bitEnd = length * 64
+						}
+						if !emit(hs|uint64(bitStart), hs|uint64(bitEnd)) {
+							return
+						}
+					}
+				}
+
+			case *arrayContainer:
+				content := t.content
+				i := 0
+				for i < len(content) {
+					start := uint64(content[i])
+					end := start + 1
+					i++
+					for i < len(content) && uint64(content[i]) == end {
+						end++
+						i++
+					}
+					if !emit(hs|start, hs|end) {
+						return
+					}
+				}
+			}
+		}
+
+		if hasPending {
+			yield(uint32(pendingStart), pendingEnd)
+		}
+	}
+}
